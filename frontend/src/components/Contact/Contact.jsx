b6fb2e438fd4f310898af76fbd8e3c2bf97c2421
darkcheftar <darkcheftar.connect@gmail.com>
2022-03-09 19:57:25 +0530

    added Contact component

---
+++ frontend/src/components/Contact/Contact.jsx
@@ -0,0 +1,17 @@
+const Contact = ()=>{
+    return (
+        <>
+            <div className="Socialmedia"></div>
+            <ul>
+                <li>Products</li>
+                <li>Our Science</li>
+                <li>Vision & Mission</li>
+                <li>About Us</li>
+            </ul>
+            <hr />
+            <input type="email" name="email" id="email" placeholder="Enter you email..." /><button>Activate</button>
+        </>
+    )
+}
+
+export default Contact;
