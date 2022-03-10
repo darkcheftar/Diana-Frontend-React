e8da18a0bb8c61d45f2ed900260679911a88e952
darkcheftar <darkcheftar.connect@gmail.com>
2022-03-10 12:52:55 +0530

    finally adding all components to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
+import About from './components/About/About';
+import Products from './components/Products/Products';
+import Contact from './components/Contact/Contact';
 function App() {
   return (
-       <Navbar/>
+    <>
+      <Navbar/>
+      <About/>
+      <Products/>
+      <div className="final-about" style={{background:" linear-gradient(180deg, #FFF6FB 0%, #E7F6FC 100%)"}}>
+      With Diana, you’re in control of your health, your every day, and your story.
+      </div> 
+      <Contact/>
+    </>
   );
 }
 
