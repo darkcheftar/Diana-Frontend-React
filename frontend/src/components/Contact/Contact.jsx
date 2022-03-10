166844953067bc6bcbd7caa8cb7902a6e43f46f6
darkcheftar <darkcheftar.connect@gmail.com>
2022-03-10 12:52:12 +0530

    styled Contact component

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -1,17 +1,31 @@
-const Contact = ()=>{
-    return (
-        <>
-            <div className="Socialmedia"></div>
-            <ul>
-                <li>Products</li>
-                <li>Our Science</li>
-                <li>Vision & Mission</li>
-                <li>About Us</li>
-            </ul>
-            <hr />
-            <input type="email" name="email" id="email" placeholder="Enter you email..." /><button>Activate</button>
-        </>
-    )
-}
+import fb from "./facebook.svg";
+import twitter from "./twitter.svg";
+import mail from "./mail.png";
+import "./Contact.scss";
+const Contact = () => {
+  return (
+    <div className="contact">
+        <div className="socialmedia">
+            <img src={fb} alt="facebook" />  
+            <img src={twitter} alt="facebook" />  
+        </div>
+      <ul className="links">
+        <li>Products</li>
+        <li>Our Science</li>
+        <li>Vision & Mission</li>
+        <li>About Us</li>
+      </ul>
+      <hr />
+      <div className="subscription">Subscribe to our Newsletter</div>
+      <input
+        type="email"
+        name="email"
+        id="email"
+        placeholder="Enter you email..."
+      />
+      <button><img src={mail} style={{paddingRight:'10px',verticalAlign:"middle"}}></img>Activate</button>
+    </div>
+  );
+};
 
 export default Contact;
